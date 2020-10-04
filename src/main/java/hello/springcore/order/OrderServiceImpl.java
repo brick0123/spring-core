d0f2c0d97a94cf096dcaa61d88fdc6892561b7f9
한진호 <tlfanfvk@naver.com>
2020-10-04 13:23:30 +0900

    새로운 할인 정책 적용

--- src/main/java/hello/springcore/order/OrderServiceImpl.java
+++ src/main/java/hello/springcore/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.springcore.order;
 
 import hello.springcore.discount.DiscountPolicy;
 import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.discount.RateDiscountPolicy;
 import hello.springcore.member.Member;
 import hello.springcore.member.MemberRepository;
 import hello.springcore.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import hello.springcore.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+    // 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
