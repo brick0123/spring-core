b1c6a573c1d57d37ed9147b3f1e708dc1e49e214
한진호 <tlfanfvk@naver.com>
2020-11-10 16:56:23 +0900

    빈 생명주기 콜백 테스트

---
+++ src/test/java/hello/springcore/lifecycle/NetworkClient.java
@@ -0,0 +1,47 @@
+package hello.springcore.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connection: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입후
+        connect();
+        call("초기화 연결 메세지");
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 빈이 종료될 때
+        disconnect();
+    }
+}
